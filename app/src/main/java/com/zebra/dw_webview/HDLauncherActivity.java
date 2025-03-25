e93d46ca2cbd00710a8ec40e093dd7cc938c4124
CXNT48 {N.DZL} <cxnt48@zebra.com>
2025-03-25 14:47:42 +0100

    v1.5-DW BT Scanner Nofify API tested

--- app/src/main/java/com/zebra/dw_webview/HDLauncherActivity.java
+++ app/src/main/java/com/zebra/dw_webview/HDLauncherActivity.java
@@ -6,31 +6,22 @@ import static android.widget.Toast.makeText;
 import static com.zebra.arcore.psspoc.helpers.BarcodeReceiverKt.dwEnumScanners;
 import static com.zebra.arcore.psspoc.helpers.BarcodeReceiverKt.registerReceiverKt;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
 import android.annotation.SuppressLint;
-import android.app.ActivityOptions;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.pm.PackageInfo;
 import android.content.res.Configuration;
 
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
 import android.provider.MediaStore;
-import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.webkit.JavascriptInterface;
@@ -39,9 +30,13 @@ import android.widget.Toast;
 
 import com.zebra.arcore.psspoc.helpers.BarcodeReceiverKt;
 
+
+
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Set;
+
+import dwwebview.DWNotifierKt;
 
 //chrome://inspect/#devices for webview debugging
 
@@ -118,6 +113,9 @@ public class HDLauncherActivity extends AppCompatActivity {
                     //acquiredbarcode = filterOutNonPrintableCharactersWIthRegex(acquiredbarcode);
                     //acquiredbarcode = filterOutNonAlphaNumericCharactersWIthRegex(acquiredbarcode);
                     //acquiredbarcode = encodeString(acquiredbarcode);
+
+                    processDWDecodeData(intent);
+
                     Log.i("onReceive", "barcode=" + acquiredbarcode + " type=" + acquiredtype);
                     //printAsciiValues(acquiredbarcode);
                     webView.loadUrl("javascript:formFill('" + acquiredbarcode + "')");
@@ -132,6 +130,17 @@ public class HDLauncherActivity extends AppCompatActivity {
         registerReceiverKt( this, broadcastReceiver)  ;
         dwEnumScanners(this);
 
+    }
+
+    void processDWDecodeData(Intent initiatingIntent){
+        ArrayList<byte[]> rawData = (ArrayList <byte[]>) initiatingIntent.getSerializableExtra("com.symbol.datawedge.decode_data");
+
+        if (rawData != null)
+        {
+            byte[] rawBytes = rawData.get(0);
+            for (int i = 0; i < rawBytes.length; i++)
+                Log.d("DW DECODE DATA", i + ": " + rawBytes[i]);
+        }
     }
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -213,11 +222,18 @@ public class HDLauncherActivity extends AppCompatActivity {
 
     public void onClickbtn_CAMERA(View v) {
         try {
-            dispatchTakePictureIntent();
+            //dispatchTakePictureIntent();
+
+            //DW NOTIFICATION EXERCISER
+            DWNotifierKt.btDWNotify(this);
+
+
         } catch (Exception e) {
             Log.e("TAG", "onClickbtn_CAMERA "+e.getMessage());
         }
     }
+
+
 
     private static int sourceVal=0;
     public void onClickbtn_SWITCH_ONE(View v) {
---
+++ app/src/main/kotlin/DWNotifier.kt
@@ -0,0 +1,34 @@
+package dwwebview
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+private const val DATAWEDGE_ACTION = "com.symbol.datawedge.api.ACTION"
+private const val NOTIFY_EXTRA = "com.symbol.datawedge.api.notification.NOTIFY"
+private const val NOTIFICATION_CONFIG = "NOTIFICATION_CONFIG"
+private const val ACTION_RESULT_DATAWEDGE_FROM_6_2 = "com.symbol.datawedge.api.RESULT_ACTION"
+private const val DEVICE_IDENTIFIER = "DEVICE_IDENTIFIER"
+private const val NOTIFICATION_SETTINGS = "NOTIFICATION_SETTINGS"
+private const val EXTRA_SEND_RESULT = "SEND_RESULT"
+private const val EXTRA_RESULT = "RESULT"
+private const val EXTRA_RESULT_INFO = "RESULT_INFO"
+private const val EXTRA_COMMAND = "COMMAND"
+
+
+    fun btDWNotify(context:Context /*notifyParams: IntArray*/)
+    {
+       // val editDeviceId = findViewById<EditText>(R.id.editDeviceId)
+        //val deviceId: String = editDeviceId.text.toString()
+        val i = Intent()
+        val bundleNotify = Bundle()
+        val bundleNotificationConfig = Bundle()
+        i.action = DATAWEDGE_ACTION;
+
+        bundleNotificationConfig.putString(DEVICE_IDENTIFIER, /*deviceId*/"BLUETOOTH_RS5100")//https://techdocs.zebra.com/datawedge/latest/guide/api/notify/
+        // bundleNotificationConfig.putIntArray(NOTIFICATION_SETTINGS, notifyParams)
+        bundleNotificationConfig.putIntArray("NOTIFICATION_SETTINGS", intArrayOf(17, 23, 8, 43))
+        bundleNotify.putBundle(NOTIFICATION_CONFIG, bundleNotificationConfig)
+        i.putExtra(NOTIFY_EXTRA, bundleNotify)
+        i.putExtra(EXTRA_SEND_RESULT, "true")
+        context.sendBroadcast(i)
+    }
